734f67f7f81f04c966099a449058d63b87404cf6
Sasha <ssbndrk@gmail.com>
2025-01-14 12:08:18 +0200

    Big update. Add new function. Now you can change task.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <string>
-int contextMenu();
 
 class work {
 private:
     std::string m_text;
     std::string task = "This excerpt calls for exploring the world, preserving one's own identity, and respecting other cultures. It beautifully conveys the wisdom and patriotism inherent in Shevchenko's work.";
 public:
-    std::string getTask() {
-        return task;
-    }
+    void setTask(const std::string &newTask) {  task = newTask; }
+    std::string getTask() { return task; }
+
     void writeTask() {
         std::cout << std::endl << "You need to rewrite this text: ";
         std::cin.ignore();
@@ -18,16 +17,21 @@ public:
         std::cout << std::endl;
         if(m_text == task) {
             std::cout << "Nice! Correct" << std::endl;
-            contextMenu();
         }
         else {
             std::cout << "Wrong!" << std::endl;
-            contextMenu();
         }
     }
 };
 
-int contextMenu() {
+int contextMenu(work &Task);
+
+int main() {
+    work Task;
+    contextMenu(Task);
+}
+
+int contextMenu(work &Task) {
     int number;
     while(true) {
         std::cout << std::endl;
@@ -38,13 +42,18 @@ int contextMenu() {
         std::cin >> number;
         switch(number) {
             case 1: {
-                work Task;
                 std::cout << Task.getTask() << std::endl;
                 Task.writeTask();
                 break;
             }
-            case 2:
+            case 2: {
+                std::string newTask;
+                std::cout << "Write the new text you want to practice: ";
+                std::cin.ignore();
+                std::getline(std::cin, newTask);
+                Task.setTask(newTask);
                 break;
+            }
             case 3:
                 std::cout << "Program is closing!" << std::endl;
                 return 0;
@@ -55,6 +64,3 @@ int contextMenu() {
     }
 }
 
-int main() {
-    contextMenu();
-}
