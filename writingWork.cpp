76d1f50fdc7633ca020c2201aaf4e0ec0fcbc1b2
kitoyaka <vlasiukdima007@gmail.com>
2025-01-16 21:36:37 +0200

    some fixes

--- writingWork.cpp
+++ writingWork.cpp
@@ -3,9 +3,11 @@
 
 void writingWork::writeTask() {
     std::cout << std::endl << "You need to rewrite this text: ";
-    writingWork example;
+
     std::cin.ignore();
     std::getline(std::cin, m_text);
+
+
     if(m_text == task) {
         setColor(2, 0);
         std::cout << "Nice! Correct" << std::endl;
@@ -14,6 +16,9 @@ void writingWork::writeTask() {
     else {
         setColor(4, 0);
         std::cout << "Wrong!" << std::endl;
+        std::cout << "Press ENTER to continue" << std::endl;
+
+
     }
     setColor(7, 0);
 }
